2bb087660fda1e3a57304085aafa5f0ce1d8cd93
hoomi_s12pro <hoomi_s12pro@debian.tom_s12pro>
2025-08-14 03:04:30 +0300

    idk just updated stuff

--- init.lua
+++ init.lua
@@ -10,7 +10,7 @@ require("config.lazy")
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
-vim.opt.smartindent = true
+vim.opt.smartindent = false 
 vim.opt.autoindent = true
 
 -- Sidebar numbers
@@ -20,5 +20,24 @@ vim.opt.relativenumber = true
 -- Sync clipboard with system
 vim.opt.clipboard = "unnamedplus"
 
-vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 
+-- Remove autocomment on enter
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+  end,
+})
+
+-- KEYBINDS --
+
+-- Remove ZZ from save and exit
+vim.keymap.set('n', 'ZZ', '<NOP>', {noremap = true, silent = true})
+
+-- Map NvimTreeToggle 
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
+
+-- Map jk to leave Insert mode  
+vim.keymap.set('i', 'jk', '<Esc>', {noremap = true, silent = true})
+
+-- Map
