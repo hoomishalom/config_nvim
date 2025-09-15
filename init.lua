b314b9d60f7dd3181b653e3d73d6b5e572cde5c1
hoomi_void <tomshabtay75@gmail.com>
2025-09-15 21:21:31 +0300

    random update

--- init.lua
+++ init.lua
@@ -40,4 +40,40 @@ vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', {noremap = true, silent 
 -- Map jk to leave Insert mode  
 vim.keymap.set('i', 'jk', '<Esc>', {noremap = true, silent = true})
 
--- Map
+-- Map Esc to :noh (stop highlighting after search)
+vim.keymap.set({'n', 'v'}, '<Esc>', ':noh<CR>', {noremap = true, silent = true})
+
+-- Telescope keybinds
+vim.keymap.set({'n', 'v'}, '<leader>ff', require('telescope.builtin').find_files, { desc = 'Telescope find files' })
+vim.keymap.set({'n', 'v'}, '<leader>fb', require('telescope.builtin').buffers, { desc = 'Telescope buffers' })
+vim.keymap.set({'n', 'v'}, '<leader>fg', require('telescope.builtin').live_grep, { desc = 'Telescope live grep' })
+
+-- Statusline --
+function _G.GetCharHex()
+  local line_content = vim.fn.getline('.')
+  local cursor_col = vim.fn.col('.')
+
+  -- Use string.sub() to get the character under the cursor
+  local char = string.sub(line_content, cursor_col, cursor_col)
+
+  if not char or char == '' then return '' end
+
+  local byte = string.byte(char)
+  return string.format(" [0x%02X] ", byte)
+end
+
+-- Define Statusline
+vim.opt.statusline = table.concat({
+  '%f ',      -- File name
+  '%y ',      -- File type
+  '%=',       -- Right align following items
+  '%{v:lua.GetCharHex()}', -- Call the global Lua function
+  '%l:%c ',   -- Line and column number
+})
+
+-- Filetypes
+vim.filetype.add({
+  extension = {
+    fasm = "fasm",
+  }
+})
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -5,7 +5,7 @@ return {
     local configs = require("nvim-treesitter.configs")
 
     configs.setup({
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "python", "cpp", "javascript", "html", "java" },
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "python", "cpp", "javascript", "html", "java", "markdown", "markdown_inline" },
       sync_install = false,
       highlight = { enable = true },
       indent = { enable = true },  
